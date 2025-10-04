0a8112af4fa2b1c02fac604d7ba68854bea7a9c1
shweta-kharade <shwetakharade2908@gmail.com>
2025-10-03 22:47:52 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+int main(){
+
+int n;
+scanf("%d",&n);
+int num=0;
+int sum=0;
+int original=n;
+int originall=n;
+while(original>0){
+   ++num;
+   original/=10;
+}
+
+while(n>0){
+   int lastdigit=n%10;
+   int powdigit = 1;
+   for(int i=1;i<=num;i++){
+       powdigit=powdigit*lastdigit;
+
+   }
+   sum=sum+powdigit;
+   n/=10;
+
+
+
+}
+
+
+if(sum==originall){
+   printf("Armstrong");
+}
+else{
+    printf("not armstrong");
+}
+
+
+
+
+
+
+
+
+
+
+}
+
