c014c35d5855c41c66f8a30f9a2e578b06bd916e
shweta-kharade <shwetakharade2908@gmail.com>
2025-10-03 22:11:51 -0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age ;
+    cout<<"Enter your age: ";
+    cin>>age;
+    
+    if(age >= 18 ){
+           cout<<"You are adult";
+    }
+    else{
+         cout<<"You are still child";
+    }
+    
+    
+    
+    
+      
+    system("pause");
+    return 0;
+}
